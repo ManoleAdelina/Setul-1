99903fcfffab647dbe7f8d092ebc1dfb9d0b1239
Adelina Manole <manoleadelina28@gmail.com>
2024-11-18 06:59:28 -0800

    A treia problema.

---
+++ Un numar se imparte la k/Un numar se imparte la k/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Un_numar_se_imparte_la_k
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, k;
+            Console.WriteLine("Numarul este: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numarul la care a se divida este: ");
+            k = int.Parse(Console.ReadLine());
+            if (a % k == 0)
+                Console.WriteLine($"Numarul se divide la {k}.");
+            else
+                Console.WriteLine($"Numarul nu se divide la {k}.");
+        }
+    }
+}
---
+++ Un numar se imparte la k/Un numar se imparte la k/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Un numar se imparte la k")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Un numar se imparte la k")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02899fef-df91-45d2-9417-aa0759a6fec1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
