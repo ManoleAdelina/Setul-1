ba6194933c0c6e016574aee6ccf200d8322f7386
Adelina Manole <manoleadelina28@gmail.com>
2024-11-18 06:30:15 -0800

    Add project files.

---
+++ Ecuatia de gradul 1 cu o necunoscuta/Program.cs
@@ -0,0 +1,19 @@
+﻿float a, b, x;
+Console.Write("a= ");
+a = float.Parse(Console.ReadLine());
+Console.Write("b= ");
+b = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Ecuatia ta este {a}x + {b} = 0");
+
+if ((a == 0) && (b == 0))
+    Console.WriteLine("Idenititate");
+else if ((a == 0) && (b != 0))
+    Console.WriteLine("Imposibil");
+else if ((a != 0) && (b == 0))
+    Console.WriteLine($"Radacina ecuatiei tale este 0");
+else if ((a != 0) && (b != 0))
+{
+    x = -b / a;
+    Console.WriteLine($"Radacina ecuatiei tale este {x}");
+}
