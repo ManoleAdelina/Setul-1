84553493f7db06db604bc55435d2173b15b2e0ac
Adelina Manole <manoleadelina28@gmail.com>
2024-11-18 07:05:24 -0800

    A patra problema

---
+++ An bisect/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace An_bisect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            Console.WriteLine("Anul este: ");
+            y=int.Parse(Console.ReadLine());
+            if (((y % 4) == 0 && (y % 100) != 0) || (y % 400) == 0)
+                Console.WriteLine("E an bisect");
+            else
+                Console.WriteLine("Nu e an bisect");
+
+                    
+        }
+    }
+}
